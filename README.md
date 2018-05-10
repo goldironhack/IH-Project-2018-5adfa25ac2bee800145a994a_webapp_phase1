62c5620107235129b5591cacbf4d3643fadee260
jrbeltranl <jrbeltranl@unal.edu.co>
2018-05-09 23:01:22 -0400

    Automatic commit. Done before go to queltrics

--- README.md
+++ README.md
@@ -1,3 +1,66 @@
-## webapp_phase1
+# Application Name
 
-undefined
+Tentative name for the application: “Hive”
+
+
+### Key Words
+Housing, Mashup, NewYorkCity
+
+### Datasets and function design
+Using the format: [name] [link] [data type] [data columns used]
+* [Neighborhood Names GIS] [https://catalog.data.gov/dataset/neighborhood-names-gis] [area] [the_geom, borough]
+*  [NY Districts geoshapes] [[http://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/nycd/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=geojson](http://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/nycd/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=geojson)] [area] [BoroCD, geometry]
+* [Crimes in NY] [[https://data.cityofnewyork.us/Public-Safety/NYPD-Complaint-Data-Historic/qgea-i56i/data](https://data.cityofnewyork.us/Public-Safety/NYPD-Complaint-Data-Historic/qgea-i56i/data)] [safety] [BORO_NM, OFNS_DESC, Lat_lon, CMPLNT_FR_DT]
+* [Dataset contains information on New York City housing by building data] [[https://catalog.data.gov/dataset/housing-new-york-units-by-building](https://catalog.data.gov/dataset/housing-new-york-units-by-building)] [housing] [borough, lat, long, Extremely Low Income Units]
+
+## Description
+
+
+The "" Project, is an application which will help to any student planning to attend to the NYU Stern who is trying to find the best district for a housing in NY . It is gonna take into account from the more basic things everybody worries about like safety, distance and price, plus some other filters the user will be able to aply in order to get the perfect match for his/her housing.
+
+Finding the best district for you to live has never been this easy!
+
+## Map View
+1.  [**Y**/N] Basic Map with specific location (your map is located in a meaningful place, city of west lafayette for example)
+**Yes:** At the moment the webpage is loaded, the map is settle down at the NYU Stern.
+
+3.  [Y/**N**] [describe] Any cover on the map (for example, cloud cover to show the weather effect)
+**No:** For the moment, there's not any cover on the map. Just a marker located on the NYU Stern.
+
+## Data Visualization
+
+1.  [**Y**/N]Use Graph? What is the type? (bar chart, pie chart, radar chart ...)
+**Yes:** For the  moment, there's some charts that show the information extracted from the datasets, the information on those charts will be used later for the construction of some other graphs related to the ranking requested.
+2.  [Y/**N**] Any interaction available on the graph? List them (enable click on numbers, drag on lines, change time/variables ...)
+**No:** There're not any interaction available on the graph yet.
+
+
+## Interaction Form
+
+
+1.  [**Y**/N] [List] Any information output? list them. (text field, text area, label, plain HTML ...)
+**Yes:** The information extracted from the datasets is showed in the charts.
+2.  [Y/**N**] [List] Any operation option (filters)? List them. (search markets, search vegetables, filter based on price, sort based on convenience ...)
+ **No:** There're not any filters available yet.
+3.  [Y/**N**] [List] Any information input? List them. (comments, markers, user preference ...)
+**No:** There're not any information inputs yet.
+4.  [Y/**N**] [List] Interaction with Map? List them. (filter on price will affect map markers, sort on price will affect map markers, ...)
+**No:** There're any interactions with Map yet.
+5.  [Y/**N**] [List] Interaction with data visualization? List them. (filter, sort, set variables ...)
+**No:** There're not any interactions with data visualization yet.
+
+## Test Cases
+The project has been tested on the next explorers:
+* Google Chrome : Works as expected.
+* IE : Internet Explorer: Google Map not working.
+* Microsoft Edge: Works as expected.
+
+
+
+## Additional information
+
+I had some problems loading some of the datasets:
+*  The GeoShapes dataset was not available as a JSON so had to Parse it. Solved.
+* The NY Crime Dataset is not working yet since the volume data is too big, I'm trying to filter the information I extract using the get function, still working on it.
+
+For the moment, I'm not taking care of the aesthetic factor, I'll try to get all the functionalities working, and then will organize and 'decorate' the page.
--- index.js
+++ index.js
@@ -1,6 +1,168 @@
 
-(function () {
-    document.getElementById("btn").addEventListener("click", function () {
-        alert("Hi! :)");
-    });
-})();
+const N_NAMES_URL ="https://data.cityofnewyork.us/api/views/xyye-rtrs/rows.json?accessType=DOWNLOAD";
+const D_GEOSHAPES_URL ="http://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/nycd/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=geojson";
+const CRIME_URL ="https://data.cityofnewyork.us/api/views/qgea-i56i/rows.json";
+const NY_HOUSING_URL ="https://data.cityofnewyork.us/api/views/hg8x-zxpr/rows.json?accessType=DOWNLOAD";
+var infoRows = [];
+var CrimeInfoRows = [];
+var GeoInfoRows = [];
+var GeoBoroInfoRows = [];
+var HousingInfoRows = [];
+var valor;
+var initialCoordenates = {lat: 40.7291, lng: -73.9965};
+
+const API_KEY = "AIzaSyCFalfoycG5PFQam2KTm9jpcUS9jkYbq0w";
+
+function initMap(){
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12  ,
+    center: initialCoordenates
+
+  });
+
+  university_marker = new google.maps.Marker({
+    position: initialCoordenates,
+    map: map
+  });
+
+}
+
+
+function getDistrictData() {
+  var data = $.get(N_NAMES_URL, function() {})
+    .done(function() {
+      console.log("Done-DistrictData");
+      console.log(data.responseJSON.data[0]);
+      var dataRow = data.responseJSON.data;
+      for (var i = 0; i < dataRow.length; i++) {
+        infoRows.push([dataRow[i][16], dataRow[i][10], dataRow[i][9]]);
+      }
+
+      var tableReference = $("#tableBody")[0];
+      var newRow, coordenates, neighborhood, borough;
+      for (var j = 0; j < infoRows.length; j++) {
+        newRow = tableReference.insertRow(tableReference.length);
+        borough  = newRow.insertCell();
+        neighborhood = newRow.insertCell();
+        coordenates = newRow.insertCell();
+
+        borough.innerHTML = infoRows[j][0]
+        neighborhood.innerHTML = infoRows[j][1];
+        coordenates.innerHTML = infoRows[j][2];
+
+      }
+
+    })
+    .fail(function (error) {
+      console.log(error);
+    })
+}
+
+function getCrimeData() {
+  console.log("BeforeDone-CrimeData");
+  var CrimeData = $.get(CRIME_URL, function() {})
+
+    .done(function() {
+      console.log(CrimeData);
+      console.log(CrimeData.responseJSON.data[0]);
+      var CrimeDataRow = CrimeData.responseJSON.data('CMPLNT_FR_DT', );
+      for(var k=0; k< CrimeDataRow.length; k++){
+        CrimeInfoRows.push([CrimeDataRow[k][13],CrimeDataRow[k][15], CrimeDataRow[k][22], CrimeDataRow[k][32]]);
+      }
+      var tableReference =$("#CrimeTableBody")[0];
+      var CrimeNewRow, CrimeDate, CrimeDescription, CrimeBorough, CrimeCoordenates;
+      for (var l=0; l<CrimeInfoRows.lenght; l++){
+        newRow = tableReference.insertRow(tableReference.length);
+        CrimeDate  = newRow.insertCell();
+        CrimeDescription = newRow.insertCell();
+        CrimeBorough = newRow.insertCell();
+        CrimeCoordenates = newRow.insertCell();
+
+        CrimeData.innerHTML = CrimeInfoRows[l][0];
+        CrimeDescription.innerHTML = CrimeInfoRows[l][1];
+        CrimeBorough.innerHTML = CrimeInfoRows[j][l];
+        CrimeCoordenates.innerHTML = CrimeInfoRows[l][3];
+      }
+
+
+  })
+  .fail(function (error) {
+    console.log("Error");
+    console.log(error);
+  })
+
+  console.log("FinCrimeData");
+}
+
+function getGeoShapesData() {
+  console.log("Before Done-GeoData");
+  var GeoData = $.get(D_GEOSHAPES_URL, function() {})
+    .done(function() {
+
+      //console.log("Done-GeoData");
+      //console.log(JSON.parse(GeoData.responseText).features[0].geometry.coordinates);
+      //console.log("Boros:");
+      //console.log(JSON.parse(GeoData.responseText).features[0].properties.BoroCD) ;
+      //console.log(JSON.parse(GeoData.responseText).features[0]);
+
+      var GeoDataRow = JSON.parse(GeoData.responseText).features;
+
+      for (var i = 0; i < GeoDataRow.length; i++) {
+        GeoInfoRows.push(GeoDataRow[i].geometry.coordinates);
+        GeoBoroInfoRows.push(GeoDataRow[i].properties.BoroCD)
+      }
+
+
+    })
+    .fail(function (error) {
+      console.log(error);
+    })
+    console.log("Fin-GeoData");
+}
+
+function getHousingData() {
+  var HousingData = $.get(NY_HOUSING_URL, function() {})
+    .done(function() {
+      console.log("Done-HousingData");
+      console.log(HousingData.responseJSON.data[0]);
+      var HousingDataRow = HousingData.responseJSON.data;
+      for (var i = 0; i < HousingDataRow.length; i++) {
+        HousingInfoRows.push([HousingDataRow[i][15], HousingDataRow[i][25], HousingDataRow[i][26], HousingDataRow[i][31]]);
+      }
+
+      var tableReference = $("#HousingTableBody")[0];
+      var newRow, borough, lat, long, ExtremeLow;
+      for (var j = 0; j < HousingInfoRows.length; j++) {
+        newRow = tableReference.insertRow(tableReference.length);
+        borough  = newRow.insertCell();
+        lat = newRow.insertCell();
+        long = newRow.insertCell();
+        ExtremeLow = newRow.insertCell();
+
+        borough.innerHTML = HousingInfoRows[j][0]
+        lat.innerHTML = HousingInfoRows[j][1];
+        long.innerHTML = HousingInfoRows[j][2];
+        ExtremeLow.innerHTML = HousingInfoRows[j][3];
+
+      }
+
+    })
+    .fail(function (error) {
+      console.log(error);
+    })
+}
+
+let getCrimes = () => {
+  fetch(CRIME_URL)
+    .then( function(response){
+      console.log(response.data)
+      ;
+    })
+}
+
+$("document").ready(function () {
+  $("#getData").on("click", getDistrictData)
+  $("#getHousingData").on("click", getHousingData)
+  $("#getCrimeData").on("click", getCrimes)
+  $("#getGeoData").on("click", getGeoShapesData)
+});
